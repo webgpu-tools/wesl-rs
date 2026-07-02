72f455cb9be8f506e2b67f152f5ed536ea3144f4
Mathis Brossier <mathis.brossier@gmail.com>
2026-07-03 01:11:20 +0200

    constructor fixes

--- crates/wgsl-types/src/tplt.rs
+++ crates/wgsl-types/src/tplt.rs
@@ -115,6 +115,9 @@ pub struct VecTemplate {
 }
 
 impl VecTemplate {
+    pub fn new_unchecked(ty: Type) -> Self {
+        Self { ty }
+    }
     pub fn parse(tplt: &[TpltParam]) -> Result<VecTemplate, E> {
         let ty = match tplt {
             [TpltParam::Type(ty)] => Ok(ty.clone()),
@@ -350,9 +353,6 @@ impl BitcastTemplate {
     }
     pub fn ty(&self) -> &Type {
         &self.ty
-    }
-    pub fn inner_ty(&self) -> Type {
-        self.ty.inner_ty()
     }
 }
 
